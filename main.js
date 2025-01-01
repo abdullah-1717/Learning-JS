b5e8c26e840440a00b90fc984daae3e62a18d574
M.Abdullah <mabdullahaheer1717@gmail.com>
2025-01-01 15:27:57 +0500

    completed

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const name = document.querySelector('#name');
 const email = document.querySelector('#email');
 const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
-init();
+let arr;
+let obj;
+oldUser();
+
 myForm.addEventListener('submit', onSubmit);
 
 function onSubmit(e)
@@ -26,66 +29,21 @@ function onSubmit(e)
         {
             arr = JSON.parse(localStorage.getItem('data'));
         }
+        obj.id = +UIDG();
         obj.name = name.value;
-       
         obj.email = email.value;
-        
-        console.log(arr);
         arr.push(obj);
         localStorage.setItem("data",JSON.stringify(arr));
-        const list = document.createElement('li');
-        const dlt = document.createElement('img');
-        dlt.src = 'delete-icon.png'; 
-        dlt.alt = 'User  Image';
-        dlt.style.marginLeft = '5px';
-        dlt.style.width = '25px'; 
-        dlt.style.height = '25px';
-        dlt.style.backgroundColor = 'white'; 
-        dlt.addEventListener('mouseover' , () => {
-            dlt.style.background = 'lightGrey';
-        });
-        dlt.addEventListener('mouseout' , () => {
-            dlt.style.background = 'white';
-        });
-        dlt.addEventListener('click', (e) => {
-            console.log({e});
-            userList.removeChild(list); 
-        });
-        const edit = document.createElement('img');
-        edit.src = 'circum-edit-icon.png'; 
-        edit.alt = 'User  Image';
-        edit.style.marginTop = 'px5';
-        edit.style.width = '25px';
-        edit.style.height = '25px';
-        edit.style.backgroundColor = 'white';
-        edit.addEventListener('mouseover' , () => {
-            edit.style.background = 'lightGrey';
-        });
-        edit.addEventListener('mouseout' , () => {
-            edit.style.background = 'white';
-        });
-        edit.addEventListener('click', () => {
-            name.value = list.childNodes[2].textContent.split(' : ')[0].trim(); 
-            email.value = list.childNodes[2].textContent.split(' : ')[1].trim(); 
-            userList.removeChild(list);
-        });
-        list.appendChild(edit);
-        list.appendChild(dlt);
-        list.appendChild(document.createTextNode(` ${name.value} : ${email.value} `));
-        userList.appendChild(list);
-        name.value = '';
-        email.value = '';
+        newUser(obj);
     }
 }
-
-function init()
+function newUser(obj)
 {
-    let oldData = JSON.parse(localStorage.getItem("data"));
-    console.log(oldData);
-    for (let i = 0; i <oldData.length ; i++){
+    
     const list = document.createElement('li');
     const dlt = document.createElement('img');
-    dlt.src = 'delete-icon.png'; 
+    dlt.src = 'delete-icon.png';
+    dlt.id = obj.id;
     dlt.alt = 'User  Image';
     dlt.style.marginLeft = '5px';
     dlt.style.width = '25px'; 
@@ -97,34 +55,130 @@ function init()
     dlt.addEventListener('mouseout' , () => {
         dlt.style.background = 'white';
     });
-    dlt.addEventListener('click', (e) => {
-        console.log({e});
-        userList.removeChild(list); 
+    dlt.addEventListener('click', () => {
+        deleteObj(list,dlt.id);
     });
     const edit = document.createElement('img');
     edit.src = 'circum-edit-icon.png'; 
     edit.alt = 'User  Image';
     edit.style.marginTop = 'px5';
+    edit.id = obj.id;
     edit.style.width = '25px';
     edit.style.height = '25px';
     edit.style.backgroundColor = 'white';
     edit.addEventListener('mouseover' , () => {
-        edit.style.background = 'lightGrey';
-    });
+    edit.style.background = 'lightGrey';});
     edit.addEventListener('mouseout' , () => {
-        edit.style.background = 'white';
-    });
+        edit.style.background = 'white';});
     edit.addEventListener('click', () => {
         name.value = list.childNodes[2].textContent.split(' : ')[0].trim(); 
         email.value = list.childNodes[2].textContent.split(' : ')[1].trim(); 
-        userList.removeChild(list);
+        deleteObj(list,edit.id);
     });
+    list.id = obj.id;
     list.appendChild(edit);
     list.appendChild(dlt);
-    
-    list.appendChild(document.createTextNode(` ${oldData[i].name} : ${oldData[i].email} `));
+    list.appendChild(document.createTextNode(` ${obj.name} : ${obj.email} `));
     userList.appendChild(list);
-    
+    name.value = '';
+    email.value = '';
+}
+
+function deleteObj(list, id){
+    let dataFromLocal = JSON.parse(localStorage.getItem("data"));
+        for (let i = 0; i < dataFromLocal.length; i++){
+            if(id == dataFromLocal[i].id){
+        dataFromLocal.splice(i,1);
+        localStorage.clear();
+        userList.removeChild(list);
+        localStorage.setItem("data",JSON.stringify(dataFromLocal));
+            }
+        }
+}
+
+function oldUser()
+{
+    let oldUsers = JSON.parse(localStorage.getItem("data"));
+    localStorage.clear();
+    for(let i = 0; i < oldUsers.length; i++){
+        obj = oldUsers[i];
+        if (localStorage.getItem("data") === null){
+            arr = [];
+        }
+        else 
+        {
+            arr = JSON.parse(localStorage.getItem('data'));
+        }
+       
+        oldDisplay(obj);
     }
-   
+}
+function oldDisplay(obj){
+    arr.push(obj);
+    localStorage.setItem("data",JSON.stringify(arr));
+    const list = document.createElement('li');
+    const dlt = document.createElement('img');
+    dlt.src = 'delete-icon.png';
+    dlt.id = obj.id;
+    dlt.alt = 'User  Image';
+    dlt.style.marginLeft = '5px';
+    dlt.style.width = '25px'; 
+    dlt.style.height = '25px';
+    dlt.style.backgroundColor = 'white'; 
+    dlt.addEventListener('mouseover' , () => {
+        dlt.style.background = 'lightGrey';
+    });
+    dlt.addEventListener('mouseout' , () => {
+        dlt.style.background = 'white';
+    });
+    dlt.addEventListener('click', () => {
+        deleteObj(list,dlt.id);
+    });
+    const edit = document.createElement('img');
+    edit.src = 'circum-edit-icon.png'; 
+    edit.alt = 'User  Image';
+    edit.style.marginTop = 'px5';
+    edit.id = obj.id;
+    edit.style.width = '25px';
+    edit.style.height = '25px';
+    edit.style.backgroundColor = 'white';
+    edit.addEventListener('mouseover' , () => {
+    edit.style.background = 'lightGrey';});
+    edit.addEventListener('mouseout' , () => {
+        edit.style.background = 'white';});
+    edit.addEventListener('click', () => {
+        name.value = list.childNodes[2].textContent.split(' : ')[0].trim(); 
+        email.value = list.childNodes[2].textContent.split(' : ')[1].trim(); 
+        deleteObj(list,edit.id);
+    });
+    list.id = obj.id;
+    list.appendChild(edit);
+    list.appendChild(dlt);
+    list.appendChild(document.createTextNode(` ${obj.name} : ${obj.email} `));
+    userList.appendChild(list);
+    name.value = '';
+    email.value = '';
+}
+
+function UIDG() {
+    const existingID = JSON.parse(localStorage.getItem("data"));
+    if(existingID == null)
+    {
+        return idGenerator();
+    }
+    let id = idGenerator();
+    for (let i = 0; i < existingID.length; i++)
+    {
+        if (id == existingID[i].id)
+        {
+            id = idGenerator();
+        }
+    }
+    return id;
+}
+
+function idGenerator()
+{
+    let uniqueNumber = Math.floor(Math.random() * 10000);
+    return uniqueNumber.toString().padStart(4, '0');
 }
